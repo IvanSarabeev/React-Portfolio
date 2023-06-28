587d11ed798699e14b25cceff3a5906b0bab3656
Ivan Sarabeev <ivan.sarabeev99@gmail.com>
2023-06-28 11:40:00 +0300

    added child component Personal, which receives props from the parent el-Resume.jsx, imported new data to the data.js file

---
+++ react-portfolio/src/components/PersonalSkills.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const SocialSkills = ({ item, activeIndex, setActiveIndex }) => {
+  const toggleDescription = (index) => {
+    setActiveIndex(index);
+  };
+
+  return (
+    <>
+      <div
+        key={item.num}
+        className={`relative mt-4 px-4 py-2 text-white shadow-2xl ${
+          item.btnVisibility ? "visible" : "hidden"
+        }`}
+      >
+        <h3 className="text-lg lg:text-xl">{`${item.num}. ${item.title}`}</h3>
+        <button
+          type="button"
+          onClick={() => toggleDescription(item.num)}
+          className={`mr-2 ${activeIndex === item.num ? "active" : ""}`}
+        >
+          <svg
+            className="absolute right-[5%] top-[5%] z-10 h-4 w-4 text-gray-800 transition-all
+                           duration-500 ease-in-out hover:top-[4%] hover:h-7 hover:w-7 hover:rounded-full hover:bg-white hover:p-[0.3rem] dark:text-white"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 8"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="m1 1 5.326 5.7a.909.909 0 0 0 1.348 0L13 1"
+            />
+          </svg>
+          {activeIndex === item.num && <p>{item.description}</p>}
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default SocialSkills;
